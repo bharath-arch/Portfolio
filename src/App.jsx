3123d0d56bf7d21420a72d183c76e7a83f3d648b
Bharath <bharathkumar100q@gmail.com>
2024-05-21 01:31:28 +0530

    final correction

--- src/App.jsx
+++ src/App.jsx
@@ -329,11 +329,12 @@ export default function App() {
               </p>
             </div>
           </div>
-          <footer className="flex flex-col p-4 mt-10 text-sm text-center text-white bg-gray-800 md:hidden">
-            Loosely designed in Figma and coded in Visual Studio Code by yours
-            truly. Built with React.js and Tailwind CSS, deployed with Vercel.
-          </footer>
+
         </div>
+        <footer className="flex flex-col p-4 mt-10 text-sm text-center text-white bg-gray-800 md:hidden">
+          Loosely designed in Figma and coded in Visual Studio Code by yours
+          truly. Built with React.js and Tailwind CSS, deployed with Vercel.
+        </footer>
       </div>
     </div>
   );
